4d35688eea9b51a85ee9cbd67bf5f8e1dfc0807e
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-01 14:51:51 +0530

    Optimized successfully

--- concatOptimized.java
+++ concatOptimized.java
@@ -1,12 +1,18 @@
 package DSA;
+import java.util.Arrays;
 
 public class concatOptimized {
     public static void main(String[] args) {
-        
+        int [] nums = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(getConcatenation(nums)));
     }
-    public static int[] concat(int[] nums){
-        
-        return nums;
+    public static int[] getConcatenation(int[] nums) {
+        int [] ans = new int [2*nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+        return ans;
     }
 }
 /*
