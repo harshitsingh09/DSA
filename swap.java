673fadb76657e60d124c6bfe491142aafc35e492
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-03 00:40:08 +0530

    switching positions of index value in an array

--- swap.java
+++ swap.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 public class swap {
     public static void main(String[] args) {
         int [] arr = {1, 23, 8, 9, 18};
-        swap(arr, 1, 3);
+        exchange(arr, 1, 3);
         System.out.println(Arrays.toString(arr));
     }
-    static void swap(int[] arr, int index1, int index2){
+    static void exchange(int[] arr, int index1, int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
