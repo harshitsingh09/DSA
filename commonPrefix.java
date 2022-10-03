d6dbdb5d500babd92a00c148a85a7540651cb511
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-03 09:49:54 +0530

    To be saved later

---
+++ commonPrefix.java
@@ -0,0 +1,15 @@
+package DSA;
+
+public class commonPrefix {
+    // public static void main(String[] args) {
+    //     String [] strs = {"flower", "float", "flight"};
+    //     String[] common = compare(strs);
+    // }
+    // public static String[] compare(String[] strs){
+    //     for (int i = 0; i < strs.length; i++) {
+    //         for (int j = 0; j < strs[i].length; j++) {
+                
+    //         }
+    //     }
+    // }
+}
