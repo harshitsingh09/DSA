dd7abc2a3de9796832e680b46a67722c2d4a2a56
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-05 00:35:00 +0530

    haywired

--- commonPrefix.java
+++ commonPrefix.java
@@ -1,15 +1,19 @@
 package DSA;
 
 public class commonPrefix {
-    // public static void main(String[] args) {
-    //     String [] strs = {"flower", "float", "flight"};
-    //     String[] common = compare(strs);
-    // }
-    // public static String[] compare(String[] strs){
-    //     for (int i = 0; i < strs.length; i++) {
-    //         for (int j = 0; j < strs[i].length; j++) {
-                
-    //         }
-    //     }
-    // }
+    public static void main(String[] args) {
+        String [] strs = {"flower", "float", "flight"};
+        String[] common = compare(strs);
+        System.out.println(common);
+    }
+    public static String[] compare(String[] strs){
+        char[] arr = new char[strs.length];
+        String[] prefix = (String) arr;
+        for (int i = 0; i < strs.length; i++) {
+            for (int j = 0; j < strs[i].length(); j++) {
+                // help
+            }
+        }
+        return 
+    }
 }
