ec0bafd509d77a3e0db4656e1ed524ba52291a3b
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-04 18:43:33 +0530

    leetcode #1929

---
+++ concatenation.java
@@ -0,0 +1,25 @@
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n 
+where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+*/
+package DSA;
+import java.util.Arrays;
+
+public class concatenation {
+    public static void main(String[] args) {
+        int [] nums = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+    }
+    public static int[] getConcatenation(int[] nums) {
+        int [] ans = new int [2*nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
