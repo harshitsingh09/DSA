f8d9dffbb04b6ceae58d4eb72ea9025fa5eb0298
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-04 23:56:10 +0530

    work in progress

---
+++ goodPair.java
@@ -0,0 +1,24 @@
+/*
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+*/
+package DSA;
+
+public class goodPair {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,1,3};
+        int output = numIdenticalPairs(nums);
+        System.out.println(output);
+    }
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if(nums[i] == nums[j]){
+                    count += 1;
+            }
+        }
+        return count;
+    }
+}
