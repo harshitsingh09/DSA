88af9215ab335d8018d19e08bb168490e7e8853a
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-04 11:58:42 +0530

    leetcode #1480

--- runningSum.java
+++ runningSum.java
@@ -1,5 +1,11 @@
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+*/
 package DSA;
 import java.util.Arrays;
+
 public class runningSum {
     public static void main(String[] args) {
         int [] nums = {12, -20, 11, 5};
