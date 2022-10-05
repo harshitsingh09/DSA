e11a2130b2303c5c3f63c3344e27b1cf2cb10d42
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-05 23:00:07 +0530

    leetcode #1512

--- goodPair.java
+++ goodPair.java
@@ -16,10 +16,7 @@ public class goodPair {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if(nums[i] == nums[j]){
-                    count += 1;
-                }
-                else{
-                    continue;
+                    count++;
                 }
             }
         }
